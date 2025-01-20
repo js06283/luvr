7f7eb215ebfc1d33696b0e09d4b53d62a95ac860
js06283 <jessicashi605@gmail.com>
2025-01-19 19:00:18 -0500

    initial commit

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,21 @@
+// navigation/AppNavigator.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+// screens/HomeScreen.js
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Welcome to SocialApp!</Text>
+    </View>
+  );
+}
